d4c1708ed5e620d327484ec2bbb9085cd82ae1ee
pauloAraujo <paulojmaraujo@gmail.com>
2012-11-10 02:49:17 +0000

    LSFnode.h

---
+++ include/LSFnode.h
@@ -0,0 +1,30 @@
+/****************************************************************************
+ * Author:      - André Freitas, p.andrefreitas@gmail.com
+ * Author:      - Paulo Araújo, paulojmaraujo@gmail.com
+ * Copyright:   - 14/10/2012, LAIG, FEUP
+ ****************************************************************************/
+
+#include "LSFprimitive.h"
+
+#ifndef LSFNODE_H_
+#define LSFNODE_H_
+
+/**
+ * LSFnode: a class that is for the nodes in the scenegraph
+ */
+class LSFnode{
+	public:
+	LSFnode(){
+		animationRef="none";
+	}
+	char *id;
+	float transformMatrix[16]; // When the node is parsed, the transformations are put in a stack
+	string appearance; // Reference to the map of appearances
+	vector<LSFprimitive> childPrimitives; // All the basic primitives
+	vector<string> childNoderefs; // All the child nodes
+	bool isDisplayList;
+	int displayList;
+	string animationRef;
+};
+
+#endif /* LSFNODE_H_ */
